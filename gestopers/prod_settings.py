3a3720aa393afc914cb66a68fd89e7299dc29d63
iam_Sangohan <mensahluc81@gmail.com>
2022-03-29 08:58:57 +0000

    Modif 4: Config staticfil

--- gestopers/prod_settings.py
+++ gestopers/prod_settings.py
@@ -5,6 +5,13 @@ import dj_database_url
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+MIDDLEWARE += [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 DATABASES['default'] = dj_database_url.config( )
 
 SECRET_KEY = 'nc+v)re0v8s!729@=db08eagk-(knm$-(i&9_0s3zm2(drh0c('
